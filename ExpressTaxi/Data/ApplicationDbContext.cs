fdbf42f96cee0240b5468be2ec01385b1996a823
“Robkata” <r.georgiev2017@pmg-pernik.com>
2022-03-01 11:46:47 +0200

    Add new roles

---
+++ ExpressTaxi/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ExpressTaxi.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressTaxi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<TaxiUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
